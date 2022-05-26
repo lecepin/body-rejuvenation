554906c07232333e18811c02d68d672d35f0300d
乐平 <383810086@qq.com>
2022-05-26 18:09:13 +0800

    Update vuepress.config.js

--- vuepress.config.js
+++ vuepress.config.js
@@ -1,8 +1,13 @@
+const { defaultTheme } = require("@vuepress/theme-default");
+
 module.exports = {
   lang: "zh-CN",
   title: "程序员回春术",
   description: "程序员回春术",
   base: "/body-rejuvenation/",
   head: [["link", { rel: "icon", href: "/body-rejuvenation/logo.png" }]],
-  repo: "https://github.com/lecepin/body-rejuvenation"
+  theme: defaultTheme({
+    repo: "https://github.com/lecepin/body-rejuvenation",
+    logo: "/logo.png",
+  }),
 };
